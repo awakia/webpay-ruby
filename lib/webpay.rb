08232fadc5057d319d9209d57ffef3c9835604a6
tomykaira <tomykaira@gmail.com>
2013-07-06 19:03:38 +0900

    Add api endpoint config

--- lib/webpay.rb
+++ lib/webpay.rb
@@ -1,5 +1,11 @@
-require "webpay/version"
+require 'webpay/version'
 
 module WebPay
-  # Your code goes here...
+  @api_base = 'https://api.webpay.jp'
+  @api_version = '/v1'
+  @api_key = nil
+
+  class << self
+    attr_accessor :api_base, :api_key
+  end
 end
