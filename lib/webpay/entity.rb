55a84daa2f058835b635b4d620a1ecadaaaba746
tomykaira <tomykaira@gmail.com>
2013-07-06 21:08:00 +0900

    Define accessors

--- lib/webpay/entity.rb
+++ lib/webpay/entity.rb
@@ -7,8 +7,23 @@ module WebPay
       end
     end
 
+    attr_reader :attributes
+
     def initialize(attributes)
       @attributes = attributes
+    end
+
+    def method_missing(method_name, *args, &block)
+      key = method_name.to_s
+      if @attributes.has_key?(key)
+        @attributes[key]
+      else
+        super
+      end
+    end
+
+    def respond_to_missing?(method_name, include_private = false)
+      @attributes.has_key?(method_name.to_s) || super
     end
   end
 end
