fd51f280221e777b421b3ad9be551a6b6bfbaa50
tomykaira <tomykaira@gmail.com>
2013-07-07 14:39:24 +0900

    Implement Charge's API

--- lib/webpay/charge.rb
+++ lib/webpay/charge.rb
@@ -1,9 +1,22 @@
 module WebPay
   class Charge < Entity
     class << self
+      def create(params = {})
+        convert(WebPay.client.post('/charges', params))
+      end
       def retrieve(id)
         convert(WebPay.client.get("/charges/#{id}"))
       end
+    end
+
+    def refund(params = {})
+      update_attributes(WebPay.client.post("/charges/#{id}/refund", params))
+    end
+
+    def update_attributes(attributes)
+      new_object = ResponseConverter.new.convert(attributes)
+      raise "unexpected object" unless new_object.is_a?(Charge)
+      @attributes = new_object.attributes
     end
   end
 end
