537379b66dd4039225c59d8b5b2ec1e37e10e87e
tomykaira <tomykaira@gmail.com>
2013-07-07 17:29:50 +0900

    EntityList provide enumeration on data
    Override count introduced by Enumeable

--- lib/webpay/entity_list.rb
+++ lib/webpay/entity_list.rb
@@ -1,8 +1,18 @@
 module WebPay
   class EntityList < Entity
+    include Enumerable
+
     def ==(other)
       other.equal?(self) ||
         (other.instance_of?(self.class) && other.data.map(&:id) == self.data.map(&:id))
+    end
+
+    def count
+      @attributes['count']
+    end
+
+    def each(&block)
+      self.data.each(&block)
     end
   end
 end
