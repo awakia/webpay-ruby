e4422f7c13984f9be69852ad8171258ef9d2e341
tomykaira <tomykaira@gmail.com>
2013-07-07 15:38:33 +0900

    Token.create, retrieve

--- lib/webpay/token.rb
+++ lib/webpay/token.rb
@@ -1,4 +1,12 @@
 module WebPay
   class Token < Entity
+    class << self
+      def create(params = {})
+        convert(WebPay.client.post('/tokens', params))
+      end
+      def retrieve(id)
+        convert(WebPay.client.get("/tokens/#{id}"))
+      end
+    end
   end
 end
