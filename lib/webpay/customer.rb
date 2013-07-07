6713c0a625c123c7eeba9163ce7b3942a3a0c80b
tomykaira <tomykaira@gmail.com>
2013-07-07 15:03:01 +0900

    Customer's class API

--- lib/webpay/customer.rb
+++ lib/webpay/customer.rb
@@ -1,4 +1,15 @@
 module WebPay
   class Customer < Entity
+    class << self
+      def create(params = {})
+        convert(WebPay.client.post('/customers', params))
+      end
+      def retrieve(id)
+        convert(WebPay.client.get("/customers/#{id}"))
+      end
+      def all(params = {})
+        convert(WebPay.client.get("/customers", params))
+      end
+    end
   end
 end
