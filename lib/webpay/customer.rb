99e5236a5d28deb2f898e7f5781f3ceb7f9a180a
tomykaira <tomykaira@gmail.com>
2013-07-07 15:33:57 +0900

    Customer's instance API

--- lib/webpay/customer.rb
+++ lib/webpay/customer.rb
@@ -11,5 +11,36 @@ module WebPay
         convert(WebPay.client.get("/customers", params))
       end
     end
+
+    attr_accessor :updated_attributes
+
+    def initialize(attributes)
+      @updated_attributes = {}
+      super(attributes)
+    end
+
+    [:description, :card, :email].each do |attr|
+      define_method("#{attr}=") do |value|
+        @updated_attributes[attr] = value
+      end
+    end
+
+    def save
+      update_attributes(WebPay.client.post("/customers/#{id}", @updated_attributes))
+      @updated_attributes = {}
+      self
+    end
+
+    def delete
+      response = WebPay.client.delete("/customers/#{id}")
+      response['deleted']
+    end
+
+    def update_attributes(attributes)
+      p attributes
+      new_object = ResponseConverter.new.convert(attributes)
+      raise "unexpected object" unless new_object.is_a?(Customer)
+      @attributes = new_object.attributes
+    end
   end
 end
