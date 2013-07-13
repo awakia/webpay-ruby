b73594b3f647b16c7fbdc7941e813c6b8d936705
tomykaira <tomykaira@gmail.com>
2013-07-13 13:17:09 +0900

    Charge documentation

--- lib/webpay/charge.rb
+++ lib/webpay/charge.rb
@@ -1,15 +1,22 @@
 module WebPay
+
+  # Object for API response hash object with <code>hash['object'] = charge</code>
   class Charge < Entity
     install_class_operations :create, :retrieve, :all
 
+    # @api private
     def self.path
       '/charges'
     end
 
+    # Refund this charge
+    # @param [Hash] params request parameters
+    # @option params [Integer] :amount The amount to refund. Default is all amount.
     def refund(params = {})
       update_attributes(WebPay.client.post([path, 'refund'].join('/'), params))
     end
 
+    # @api private
     def path
       "/charges/#{id}"
     end
