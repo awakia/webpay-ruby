4711b4abc58fd1521e65804cba34ed757eb4d1a1
tomykaira <tomykaira@gmail.com>
2013-07-13 13:12:39 +0900

    Card documentation

--- lib/webpay/card.rb
+++ lib/webpay/card.rb
@@ -1,5 +1,11 @@
 module WebPay
+
+  # Object for API response hash object with <code>hash['object'] = card</code>.
+  # Card is not accessible as an API endpoint
   class Card < Entity
+
+    # Check equality with rhs
+    # @return [Boolean] true if rhs is the same object, or it is a Card with the same fingerprint value
     def ==(other)
       other.equals?(self) || (other.instance_of?(self.class) && other.fingerprint == self.fingerprint)
     end
