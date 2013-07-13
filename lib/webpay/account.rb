0b8d1eda3311bd8e07c25d4ff7f63c136d638a09
tomykaira <tomykaira@gmail.com>
2013-07-13 13:09:19 +0900

    Account documentation

--- lib/webpay/account.rb
+++ lib/webpay/account.rb
@@ -1,10 +1,17 @@
 module WebPay
+
+  # Object for API response hash object with <code>hash['object'] = account</code>
   class Account < Entity
     class << self
+
+      # Get the account of api_key's owner
+      # @return [Account] the account of api_key's owner
       def retrieve
         convert(WebPay.client.get(path))
       end
 
+      # @api private
+      # @return [String] Relative path to API root: <code>'/account'</code>
       def path
         '/account'
       end
