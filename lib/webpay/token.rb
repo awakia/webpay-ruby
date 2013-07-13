aef478bb567d736ccde2f98452944677d3c288dc
tomykaira <tomykaira@gmail.com>
2013-07-13 13:51:15 +0900

    Token documentation

--- lib/webpay/token.rb
+++ lib/webpay/token.rb
@@ -1,7 +1,10 @@
 module WebPay
+
+  # Object for API response hash object with <code>hash['object'] = token</code>
   class Token < Entity
     install_class_operations :create, :retrieve
 
+    # @api private
     def self.path
       '/tokens'
     end
