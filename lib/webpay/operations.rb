6a34ec3273bdd552bfe4c9309236df738ac090cc
tomykaira <tomykaira@gmail.com>
2013-07-13 13:48:35 +0900

    Operations documentation

--- lib/webpay/operations.rb
+++ lib/webpay/operations.rb
@@ -1,11 +1,19 @@
 module WebPay
+
+  # This module installs collection operations to entities.
+  # Some of <code>create</code>, <code>retrieve</code>, and <code>all</code> are defined as class methods.
   module Operations
+
+    # Install selected operations
+    # @api private
     def install_class_operations(*operations)
       define_create if operations.include?(:create)
       define_retrieve if operations.include?(:retrieve)
       define_all if operations.include?(:all)
     end
 
+    # Define <code>create</code> method
+    # @api private
     def define_create
       instance_eval do
         def create(params = {})
@@ -14,6 +22,8 @@ module WebPay
       end
     end
 
+    # Define <code>retrieve(id)</code> method
+    # @api private
     def define_retrieve
       instance_eval do
         def retrieve(id)
@@ -26,6 +36,8 @@ module WebPay
       end
     end
 
+    # Define <code>all</code> method
+    # @api private
     def define_all
       instance_eval do
         def all(params = {})
