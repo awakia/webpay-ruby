7cded4fb7c9e08a867a9614a282250c6362bc78c
tomykaira <tomykaira@gmail.com>
2013-07-13 13:56:57 +0900

    WebPay module document

--- lib/webpay.rb
+++ lib/webpay.rb
@@ -1,5 +1,7 @@
 require 'webpay/version'
 
+# Toplevel module of WebPay gem.
+# This is the start point.
 module WebPay
   autoload(:Client, 'webpay/client')
   autoload(:Operations, 'webpay/operations')
@@ -24,28 +26,39 @@ module WebPay
   @api_key = nil
 
   class << self
+    # Absolute path to SSL CA file.
+    # This gem includes SSL CA file as lib/data/ca-certificates.crt.
     def ssl_ca_file
       File.join(File.dirname(File.expand_path(__FILE__)), 'data', 'ca-certificates.crt')
     end
 
+    # Current client object.
+    # client is memoized, and nullified when @api_base or @api_key is modified.
     def client
       @client ||= Client.new(@api_key, @api_base, @api_version)
     end
 
+    # Set api_base, the base URL of API.
+    # Configure this before sending any request.
+    # Take care when using unofficial WebPay API.
     def api_base=(new_value)
       @api_base = new_value
       @client = nil
     end
 
+    # Set api_key, your token for accessing API.
+    # Configure this before sending any request.
     def api_key=(new_value)
       @api_key = new_value
       @client = nil
     end
 
+    # Get current api_base
     def api_base
       @api_base
     end
 
+    # Get current api_key
     def api_key
       @api_key
     end
