a345789e754496a90b3596304f961d3ce3791f54
tomykaira <tomykaira@gmail.com>
2013-07-13 13:45:31 +0900

    Event documentation

--- lib/webpay/event.rb
+++ lib/webpay/event.rb
@@ -1,11 +1,17 @@
 module WebPay
+
+  # Object for API response hash object with <code>hash['object'] = event</code>
   class Event < Entity
     install_class_operations :retrieve, :all
 
+    # @api private
     def self.path
       '/events'
     end
 
+    # Entity object for <code>event.data</code>
+    # This entity has <code>object</code> on which event occurred,
+    # and <code>previous_attributes</code> if some of attributes are modified on the event.
     class Data < Entity
     end
   end
